ae44cd9d0082eba3a602f313e72ba24f25f4ecdb
Lxs_ <112419013+Lxs0ff@users.noreply.github.com>
2025-04-22 20:28:37 -0400

    Form Spammer v2 Works with esc key as backup safety

--- FormSpammer2.py
+++ FormSpammer2.py
@@ -1,34 +1,47 @@
 import subprocess as sp
 sp.run("pip install pynput")
 from pynput.mouse import Controller,Button,Listener
-import pynput
+import keyboard
 import time
 
 pos = []
 idx = 0
 count = 0
 num = 4
+t1 = 0
+t2 = 0
+delay = 0.4
 
 mouse = Controller()
-
-time.sleep(1)
 
 def on_click(x, y, button, pressed):
     global pos
     if button.name == 'left' and pressed:
         pos.append(mouse.position)
-    if len(pos) == 9: 
+        print(f"Click #{len(pos)} is now set")
+    if len(pos) == num+2: 
         listener.stop()
 
 with Listener(on_click=on_click) as listener:
     listener.join()
 
+print("Starting in 3 seconds ...")
+
 time.sleep(3)
 
-
-while count < num+2:
-    mouse.position = pos[idx]
-    mouse.click(Button.left,1)
-    idx+=1
-    if idx > len(pos) - 1 : idx = 0;count+=1;
-    if pynput.keyboard.Controller.pressed(pynput.keyboard.Key.tab):exit()
+t1 = time.time()
+running = True          
+while running:
+    t2 = time.time()
+    if t2-t1>=delay:
+        t1 = time.time()
+        try:
+            mouse.position = pos[idx]
+        except:
+            idx = 0
+            count+=1
+            mouse.position = pos[idx]
+            print(f"Cycle #{count}")
+        mouse.click(Button.left,1)
+        idx+=1
+    if keyboard.is_pressed('escape'):running=False
