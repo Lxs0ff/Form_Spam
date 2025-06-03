6213f5212e79841870bba589aa64608ccb4aedb3
Lxs_ <112419013+Lxs0ff@users.noreply.github.com>
2025-06-03 19:18:14 -0400

    Text Update for FormSpammer 2

--- FormSpammer2.py
+++ FormSpammer2.py
@@ -7,10 +7,11 @@ import time
 pos = []
 idx = 0
 count = 0
-num = 4
+num = 1
 t1 = 0
 t2 = 0
 delay = 0.4
+_ = False
 
 mouse = Controller()
 
@@ -19,8 +20,21 @@ def on_click(x, y, button, pressed):
     if button.name == 'left' and pressed:
         pos.append(mouse.position)
         print(f"Click #{len(pos)} is now set")
-    if len(pos) == num+2: 
+    if len(pos)-pos.count("v") == num+2: 
         listener.stop()
+    
+def on_key_event(event):
+    global _,pos
+    if event.name == 'v' and keyboard.is_pressed('ctrl') and _ == False:
+        _ = True
+        pos.append("v")
+
+def ctrl_release(a):
+    global _
+    _= False
+
+keyboard.on_release(ctrl_release)
+keyboard.hook(on_key_event)
 
 with Listener(on_click=on_click) as listener:
     listener.join()
@@ -35,13 +49,14 @@ while running:
     t2 = time.time()
     if t2-t1>=delay:
         t1 = time.time()
-        try:
-            mouse.position = pos[idx]
-        except:
+        if 0 < idx >= len(pos):
             idx = 0
             count+=1
-            mouse.position = pos[idx]
             print(f"Cycle #{count}")
-        mouse.click(Button.left,1)
+        if type(pos[idx]) != str:
+            mouse.position = pos[idx]
+            mouse.click(Button.left,1)
+        else:
+            keyboard.press_and_release("ctrl+v")
         idx+=1
     if keyboard.is_pressed('escape'):running=False
